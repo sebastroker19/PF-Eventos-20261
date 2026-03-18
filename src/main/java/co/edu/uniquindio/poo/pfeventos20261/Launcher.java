6e0f1a9bf2bbb8c19477a8ea8a641789d8f86d7f
Sebastroker <sebastiana.silvab@uqvirtual.edu.co>
2026-03-18 09:40:53 -0500

    0% trabajo

---
+++ src/main/java/co/edu/uniquindio/poo/pfeventos20261/Launcher.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.poo.pfeventos20261;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module co.edu.uniquindio.poo.pfeventos20261 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.poo.pfeventos20261 to javafx.fxml;
+    exports co.edu.uniquindio.poo.pfeventos20261;
+}
