2817dc6208a21a34fa244f5c5d41e244b6bbe1fb
Kevin3475 <kevine.echeverryv@uqvirtual.edu.co>
2026-04-26 08:51:46 -0500

    3 Patron Clase Empresa

---
+++ src/main/java/co/edu/uniquindio/poo/proyectofinal/model/Empresa.java
@@ -0,0 +1,51 @@
+package co.edu.uniquindio.poo.proyectofinal.model;
+
+public final class Empresa {
+
+    //Instancia de la Clase
+
+    private static Empresa instance;
+
+    //Atributos Propios de la Clase
+
+    private String nombre;
+    private String nit;
+
+
+    //Constructor
+
+    private Empresa(String nombre, String nit){
+        this.nombre = nombre;
+        this.nit = nit;
+
+    }
+
+    //Metodo getInstance Propio del patron Singleton
+
+    public static Empresa getInstance(){
+
+        if(instance == null){
+            instance = new Empresa("SoluEmpresa","jdk42");
+        }
+        return instance;
+    }
+
+
+    //Getters y Setters
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
